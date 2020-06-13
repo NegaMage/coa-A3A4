e253335a470abe21201b3684559ffc70505b23b5
Vignesh Srinivasan <vignesh1722@gmail.com>
2020-06-13 20:29:30 +0530

    control unit for upower

--- uPower/Code/Control_Unit.v
+++ uPower/Code/Control_Unit.v
@@ -1,3 +1,4 @@
+`timescale 1ns/1ns
 /* Module to behave like the control unit to set the necessary signals for the execution of an 
    instruction -
    1. RegDst - Which field of the instruction is the register to be written to
@@ -23,6 +24,15 @@ module control_unit(
     input [1:0] xods
 );
 
+    // always @(opcode, xoxo, xox, xods)
+    // begin
+    //     MemRead = 1'b0;
+    //     MemWrite = 1'b0;
+    //     RegRead = 1'b0;
+    //     RegWrite = 1'b0;
+    //     Branch = 1'b0;
+    // end
+    
     always @(opcode, xoxo, xox, xods)
     begin
         MemRead = 1'b0;
@@ -30,10 +40,6 @@ module control_unit(
         RegRead = 1'b0;
         RegWrite = 1'b0;
         Branch = 1'b0;
-    end
-    
-    always @(opcode, xoxo, xox, xods)
-    begin
         //X OR XO Format
         if(opcode == 6'd31) 
         begin
@@ -75,5 +81,64 @@ module control_unit(
             Branch = 1'b1;
             RegRead = 1'b1;
         end
+    end
+endmodule
+
+module control_unit_tb();
+    wire RegRead, RegWrite, MemRead, MemWrite, Branch;
+    reg [5:0] opcode;
+    reg [9:0] xox;
+    reg [8:0] xoxo; 
+    reg [1:0] xods;
+
+    control_unit controller(
+        .RegRead(RegRead),
+        .RegWrite(RegWrite),
+        .MemRead(MemRead),
+        .MemWrite(MemWrite),
+        .Branch(Branch),
+        .opcode(opcode),
+        .xox(xox),
+        .xoxo(xoxo),
+        .xods(xods)
+    );
+
+    initial begin
+        //add - XO
+        opcode = 6'd31;
+        xoxo = 9'd266;
+        #10;
+
+        //ld - DS
+        opcode = 6'd58;
+        xods = 2'd0;
+        #10;
+        
+        //ba - I
+        opcode = 6'd18;
+        #10;
+        
+        //and - X
+        opcode = 6'd31;
+        xox = 10'd28;
+        #10;
+        
+        //bc - B
+        opcode = 6'd19;
+        #10;
+        
+        //bl - I
+        opcode = 6'd18;
+        #10;
+        
+        //xori - D
+        opcode = 6'd26;
+        #10;
+        
+    end
+
+    initial begin
+        $dumpfile("control_unit_tb.vcd");
+        $dumpvars(0,control_unit_tb);
     end
 endmodule
